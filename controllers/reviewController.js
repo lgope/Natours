7786acc17241f93d46e7b84663f051bbd835bdda
lgope <ilakshmangope@gmail.com>
2019-12-09 18:27:33 +0600

    All Controllers 🗽🛠

---
+++ controllers/reviewController.js
@@ -0,0 +1,16 @@
+const Review = require('./../models/reviewModel');
+// const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
+exports.getAllReviews = factory.getAll(Review);
+exports.getlReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);

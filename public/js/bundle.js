5c8912ada74336e32867c5aa68cdd8dc1566e390
lgope <ilakshmangope@gmail.com>
2020-04-16 13:31:09 +0600

    fixed a bug. 🛠

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9026,7 +9026,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1899" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5480" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

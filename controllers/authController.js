246c3f5893ed0cb077263ac1a961f9cb3609760b
lgope <ilakshmangope@gmail.com>
2023-02-14 20:51:44 +0600

    fixed sharp issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,7 +42,6 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   const url = `${req.protocol}://${req.get('host')}/me`;
-  // console.log(url);
   await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, req, res);
